483c44c737d3010ed364b80b7777ae0875e8ba72
Morten Trydal <trydis@hotmail.com>
2015-04-17 14:13:27 +0200

    Added lifetime disposal detector

---
+++ Whitebox.Core/Analytics/LifetimeDisposal/LifetimeDisposalDetector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Whitebox.Core.Application;
+
+namespace Whitebox.Core.Analytics.LifetimeDisposal
+{
+    class LifetimeDisposalDetector : IApplicationEventHandler<ItemCompletedEvent<LifetimeScope>>
+    {
+        readonly IApplicationEventQueue _applicationEventQueue;
+
+        public LifetimeDisposalDetector(IApplicationEventQueue applicationEventQueue)
+        {
+            if (applicationEventQueue == null) throw new ArgumentNullException("applicationEventQueue");
+            _applicationEventQueue = applicationEventQueue;
+        }
+
+        public void Handle(ItemCompletedEvent<LifetimeScope> applicationEvent)
+        {
+            var lifetimeScope = applicationEvent.Item;
+            var messageEvent = new MessageEvent(MessageRelevance.Information, "Lifetime scope disposed", lifetimeScope.Description);
+            _applicationEventQueue.Enqueue(messageEvent);
+        }
+    }
+}
